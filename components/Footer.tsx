678b27d76e7588e33b8875bb1ffb4cd4d68f0f6e
parisabrz <49725111+parisabrz@users.noreply.github.com>
2024-10-20 11:41:34 +0330

    change some files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
@@ -33,7 +34,12 @@ function Footer() {
               key={profile.id}
               className="w-10 h-10  cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img
+                src={profile.img}
+                alt={profile.id.toString()}
+                width={20}
+                height={20}
+              />
             </div>
           ))}
         </div>
